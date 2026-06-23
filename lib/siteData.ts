75a9c73a6441c1c828f797e5b60e562897337720
Joy Das <joydas.0111@gmail.com>
2026-06-23 20:17:46 +0530

    Committing the site data

---
+++ lib/siteData.ts
@@ -0,0 +1,22 @@
+export const advocate = {
+  name: "Sudipto Panda",
+  designation: "Advocate",
+  court: "Calcutta High Court",
+  experience: "34 years",
+  address: "200/E, Shyama Prasad Mukherjee Road, Kolkata 700026",
+  phone: "9433007000",
+  whatsapp: "9874452313",
+  email: "sudiptopanda@gmail.com",
+};
+
+export const practiceAreas = [
+  "Civil matters relating to property",
+  "Banking matters",
+  "Matrimonial matters before High Courts and Trial Courts",
+  "Conveyances and Registration",
+  "Contract Drafting",
+  "Criminal matters, Bail, Revision and Appeal",
+  "Writ Matters in High Court",
+  "Arbitration proceedings",
+  "All Tribunal applications",
+];
